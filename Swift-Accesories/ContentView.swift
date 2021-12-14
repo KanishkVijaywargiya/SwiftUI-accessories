581143f462743c3d1159936405c2fdeabf293751
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-12-14 18:10:02 +0530

    Initial Commit

---
+++ Swift-Accesories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-Accesories
+//
+//  Created by KANISHK VIJAYWARGIYA on 14/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Accesories/Swift_AccesoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AccesoriesApp.swift
+//  Swift-Accesories
+//
+//  Created by KANISHK VIJAYWARGIYA on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AccesoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
